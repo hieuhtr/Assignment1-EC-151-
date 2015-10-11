bd38e30ef222c0eb89b5cc349c10090948ce7743
hieubac1511 <hieubac1511@gmail.com>
2015-10-11 18:29:43 +0700

    Hieunq:
    test FB API

--- js/script.js
+++ js/script.js
@@ -469,18 +469,38 @@ $(document).ready(function(){
             element.parentNode.removeChild(element);
         },true);
         document.getElementById('shareBtn').addEventListener('click',function(){
-            FB.api(
-                '/me/feed', 
-                'post',
-                {message: "I've reached "+score+" in Snake game. Play with me at: http://snake-game.co.nr/"},
-                function(response){
-                    console.log(response);
-                    if (!response || response.error) {
-                        alert('You must login to share this game with your friends');
-                    } 
-                    else {
-                        alert('Successful!');
-                    }
+            FB.getLoginStatus(function(response){
+                if(response.status == 'connected'){
+                    FB.api(
+                        '/me/feed', 
+                        'post',
+                        {message: "I've reached "+score+" in Snake game. Play with me at: http://snake-game.co.nr/"},
+                        function(response){
+                            console.log(response);
+                            if (!response || response.error) {
+                                alert('You must login to share this game with your friends');
+                            } 
+                            else {
+                                alert('Successful!');
+                            }
+                    });
+                }
+                else{
+                    FB.login();
+                    FB.api(
+                        '/me/feed', 
+                        'post',
+                        {message: "I've reached "+score+" in Snake game. Play with me at: http://snake-game.co.nr/"},
+                        function(response){
+                            console.log(response);
+                            if (!response || response.error) {
+                                alert('You must login to share this game with your friends');
+                            } 
+                            else {
+                                alert('Successful!');
+                            }
+                    });
+                }
             });
         });
     });
